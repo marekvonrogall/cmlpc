a10a41c72bf6f7233683054aba66fc86c70436c3
Marek Mika von Rogall <marekvonrogall@gmx.ch>
2024-12-24 14:22:03 +0100

    Initial commit. Add ASP .NET API. Add container structure. Add CI-Pipeline setup.

---
+++ src/PriceCalculator/Controllers/CalculationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PriceCalculator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculationController : ControllerBase
+    {
+        [HttpGet(Name = "calculate")]
+        public string Get()
+        {
+            return "works";
+        }
+    }
+}
